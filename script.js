06460eae0f13d2ea4a92a1259e8d7b62ff4e8338
Mel Huang <hello@melaniehuang.com>
2025-09-01 16:02:23 +1000

    testing location ar

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/bboots.gltf');
-        model.setAttribute('rotation', '0 180 0');
-        model.setAttribute('position', '0 0 0');
-        //model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.1 0.1 0.1');
+        model.setAttribute('gltf-model', './assets/magnemite/red-boots.gltf');
+        //model.setAttribute('rotation', '0 180 0');
+        //model.setAttribute('position', '0 0 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '1 1 1');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
