d29bd050c8d34bab37f6463d6a0086d0f682461f
Mel Huang <hello@melaniehuang.com>
2025-09-01 16:06:05 +1000

    testing location ar

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function renderPlaces(places) {
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', './assets/magnemite/bboots.gltf');
         //model.setAttribute('rotation', '0 180 0');
-        //model.setAttribute('position', '0 0 0');
+        model.setAttribute('position', '20 0 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.25 0.25 0.25');
 
